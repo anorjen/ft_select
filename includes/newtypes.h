f155129da941605d11cf49f055e1c305e9f77815
anorjen <1qwert147@rambler.ru>
2020-02-09 22:03:08 +0300

    add header

--- includes/newtypes.h
+++ includes/newtypes.h
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newtypes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anorjen <anorjen@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/09 21:59:36 by anorjen           #+#    #+#             */
+/*   Updated: 2020/02/09 22:01:00 by anorjen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef NEWTYPES_H
 # define NEWTYPES_H
-// # include "doing.h"
 
-typedef struct	s_color
+typedef struct		s_color
 {
-	char		*name;
-	char		*color;
-}				t_color;
+	char			*name;
+	char			*color;
+}					t_color;
 
-typedef enum	e_keycode
+typedef enum		e_keycode
 {
 	ZERO = 0,
 	ENTER = 10,
@@ -20,16 +31,16 @@ typedef enum	e_keycode
 	RIGHT = 4414235,
 	LEFT = 4479771,
 	DELETE = 2117294875L
-}				t_keycode;
+}					t_keycode;
 
-typedef struct	s_term
+typedef struct		s_term
 {
-	char		*cl;
-	char		*vi;
-	char		*ve;
-	char		*ti;
-	char		*te;
-}				t_term;
+	char			*cl;
+	char			*vi;
+	char			*ve;
+	char			*ti;
+	char			*te;
+}					t_term;
 
 typedef struct		s_arg
 {
@@ -50,50 +61,12 @@ typedef struct		s_select
 	int				mark_size;
 }					t_select;
 
-// //colors.c
-// t_color	g_colors[12] = {
-// 	{"black", "\033[30m"},
-// 	{"red", "\033[31m"},
-// 	{"green", "\033[32m"},
-// 	{"yellow", "\033[33m"},
-// 	{"blue", "\033[34m"},
-// 	{"purple", "\033[35m"},
-// 	{"cyan", "\033[36m"},
-// 	{"white", "\033[37m"},
-// 	{"normal", "\033[0m"},
-// 	{"inverse", "\033[4m"},
-// 	{"underline", "\033[7m"},
-// 	{NULL, NULL}
-// };
+typedef int			(*t_spe_key) (t_select *select);
 
-// t_color	g_type_colors[6] = {
-// 	{"c", "blue"},
-// 	{"h", "cyan"},
-// 	{"o", "green"},
-// 	{"pdf", "red"},
-// 	{"doc", "yellow"},
-// 	{NULL, NULL}
-// };
-
-typedef int		(*t_spe_key) (t_select *select);
-
-typedef struct	s_action
+typedef struct		s_action
 {
-	t_keycode	key;
-	t_spe_key	action;
-}				t_action;
-
-// t_action	g_key_actions[10] = {
-// 	{LEFT, do_left},
-// 	{RIGHT, do_right},
-// 	{UP, do_up},
-// 	{DOWN, do_down},
-// 	{SPACE, do_mark},
-// 	{ENTER, do_confirm},
-// 	{ESC, do_escape},
-// 	{DELETE, do_delete},
-// 	{BACKSPACE, do_delete},
-// 	{ZERO, NULL}
-// };
+	t_keycode		key;
+	t_spe_key		action;
+}					t_action;
 
 #endif
